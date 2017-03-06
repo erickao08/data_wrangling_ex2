32ab3bf7abbe4dd377a65ea7add2d824b7fae9be
erickao08 <erickao08@hotmail.com>
2017-03-05 22:47:13 -0600

    Finish the rest of the question

--- Data Wrangling Exercise 2.R
+++ Data Wrangling Exercise 2.R
@@ -8,3 +8,16 @@ structure(titanic)
 
 #emabark missing value
 titanic$embarked[is.na(titanic$embarked)]<-'S'
+
+
+#age missing value
+m<-mean(titanic$age,na.rm=TRUE)
+titanic$age[is.na(titanic$age)]<-m
+#it is okay to have the mean to fill up the null value. It will not effect the population distribution shape too much. 
+
+
+#boat missing value
+titanic$boat[is.na(titanic$boat)]<-'None'
+
+#cabin missing value
+#I do not think it is necessary to fill the empty value here since at the time not everyone on the ship is rich enough to afford a cabin. 
