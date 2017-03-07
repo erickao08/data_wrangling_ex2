7eebe8fec291924ad133c61f7148eb9dc6d60ede
erickao08 <erickao08@hotmail.com>
2017-03-06 21:59:41 -0600

    has_cabin_number and cabin number explained

--- Data Wrangling Exercise 2.R
+++ Data Wrangling Exercise 2.R
@@ -13,11 +13,20 @@ titanic$embarked[is.na(titanic$embarked)]<-'S'
 #age missing value
 m<-mean(titanic$age,na.rm=TRUE)
 titanic$age[is.na(titanic$age)]<-m
-#it is okay to have the mean to fill up the null value. It will not effect the population distribution shape too much. 
+
+#It will change the distribution of the population; however it won't change the mean. 
+#I think it's okay to use the mean to fill the null unless you're trying to get the mode. 
 
 
 #boat missing value
 titanic$boat[is.na(titanic$boat)]<-'None'
 
 #cabin missing value
-#I do not think it is necessary to fill the empty value here since at the time not everyone on the ship is rich enough to afford a cabin. 
+#I do not think it is necessary to fill the empty value here since at the time not everyone on the ship is rich enough to afford a cabin.
+#Some might not have a cabin and filling the cabin number with a filled value will twist the fact. 
+
+#has_cabin_number 
+titanic$has_cabin_number[is.na(titanic$cabin)]<-0
+titanic$has_cabin_number[!(is.na(titanic$cabin))]<-1
+
+
